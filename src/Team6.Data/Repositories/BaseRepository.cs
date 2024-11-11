10642bc98464602c46d8beef2150458c49b059c1
Joseph Berret <berretashton@gmail.com>
2024-11-11 10:35:27 -0600

    Fixed some dependency issues

--- src/Team6.Data/Repositories/BaseRepository.cs
+++ src/Team6.Data/Repositories/BaseRepository.cs
@@ -1,5 +1,8 @@
 using System.Data;
 using System.Collections.Generic;
+using System.Threading.Tasks; 
+using Dapper;
+using Team6.Core.Models;
 using Team6.Data.Context;
 
 namespace Team6.Data.Repositories
