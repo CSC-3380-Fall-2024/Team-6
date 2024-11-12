f31171a7a7d7cf22aa9d7a2849e17cab3c8d644a
Joseph Berret <berretashton@gmail.com>
2024-11-12 09:20:11 -0600

    Updated User model

--- src/Team6.Core/Models/User.cs
+++ src/Team6.Core/Models/User.cs
@@ -8,6 +8,14 @@ namespace Team6.Core.Models
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime LastLogin { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? LastPasswordChange { get; set; }
+        public int numFailedLogins { get; set; }
+        public DateTime? LockedOutEnd { get; set; }
+
+        // navigation 
+        public List<CalendarEvent>? CalendarEvents { get; set; }
+        public List<Reflection>? Reflections { get; set; }
+        public ThemeSetting? ThemeSetting { get; set; }
     }
 }
